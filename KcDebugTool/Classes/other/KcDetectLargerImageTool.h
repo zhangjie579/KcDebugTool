a54bf8f2c4a37e8705c6a7aa16cd0ff03c922a55
张杰 <527512749@qq.com>
2023-04-23 15:32:30 +0800

    fix: 大图check的头文件

--- KcDebugTool/Classes/other/KcDetectLargerImageTool.h
+++ KcDebugTool/Classes/other/KcDetectLargerImageTool.h
@@ -13,7 +13,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 + (void)start;
 
-+ (void)startWithImageInfoBlock:(NSString *(*)(UIImageView *))imageInfoBlock;
++ (void)startWithImageInfoBlock:(NSString *_Nullable(*_Nullable)(UIImageView *))imageInfoBlock;
 
 @end
 
